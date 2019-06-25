b8df6e5aceb869c462a5172c952af48002f0ef5c
maxiangsai <maxiangsai@163.com>
2019-06-25 08:14:58 +0800

    init

--- config/index.js
+++ config/index.js
@@ -2,12 +2,5 @@ require('env2')('./.env');
 const { env } = process;
 module.exports = {
   host: env.HOST,
-  port: env.PORT,
-  jwt: {
-    secret: env.JWT_SECRET,
-    expiresIn: Math.floor(new Date().getTime() / 1000) + 1 * 24 * 60 * 60
-  },
-  mongo: {
-    host: env.MONGO_URI
-  }
+  port: env.PORT
 }
